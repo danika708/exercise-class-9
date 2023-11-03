c59a23caf465715bae6e327b00326b029ebd9a96
Danika Durano <danikadurano@Danikas-MacBook-Air.local>
2023-11-03 15:19:50 -0700

    added/reworded questions

--- 2-arrays/index.js
+++ 2-arrays/index.js
@@ -10,8 +10,14 @@
 
 /**
  * Question 5 - concat two different arrays by using .concat() method
+ * -----------
+ * initialise the variable called `animals` to a 2-Dimensional array containing
+ * the values below:
+ *          C[0]    C[1]    C[2]
+ *  Row 1   cat     dog     hamster
+ *  Row 2   frog    lizard  horse
  * 
- *  
+ * then use the .concat() method to console log out a list
  */
 
 var animals = [
@@ -29,8 +35,16 @@ console.log(animalList)
 /**
  * Question 6 - create a 2 dimensions array that is 2-by-4. 
  * Grab some values out of this array to create a sentence using concatenation.
- * 
+ * ----------
+ * initialise the variable called `food` to a 2-Dimensional array conataining 
+ * the values below:
+ *          column(0)   column(1)   column(2)   column(3)
+ * Row 1    vanilla     chocolate   strawberry  mango
+ * Row 2    red         yellow      blue        green
  *  
+ * then based on the values stored in the food array, console log out the following
+ * sentence (use concatenation):
+ * For my cake I want _flavour_ with _color_ icing.
  */
 
 var food = [
@@ -39,5 +53,5 @@ var food = [
 ]
 
 
-var cakeOrder = "For my cake i want " + cakeFlavours[1] + " with " + icingColor[2] + " icing."
+var cakeOrder = "For my cake I want " + cakeFlavours[1] + " with " + icingColor[2] + " icing."
 console.log(cakeOrder)
